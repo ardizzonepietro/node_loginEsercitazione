78543ad5b99fef51ffcfe775113eddae79b62a51
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-02-03 11:00:47 +0100

    render login page

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = 800
 app.set('views', "./views")
 app.set("view engine", "ejs")
 
-app.post("/", (req, res)=>{
-    res.render("views/index.ejs")
+app.get("/", (req, res)=>{
+  res.render("index.ejs")
+  
 })
-
 app.listen(port, ()=>console.info("sto ascoltandando"))
