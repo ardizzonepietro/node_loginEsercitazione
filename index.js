ba7e939f5e4f31843a6bd5eafd43047f368d82d3
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-02-03 11:22:38 +0100

    fix

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const app = express()
 const port = 800
+let jsonFile = require('jsonfile');
 app.set('views', "./views")
 app.set("view engine", "ejs")
 
 app.get("/", (req, res)=>{
   res.render("index.ejs")
   
+})
+app.post("/index.ejs", (req, res)=>{
+
 })
 app.listen(port, ()=>console.info("sto ascoltandando"))
