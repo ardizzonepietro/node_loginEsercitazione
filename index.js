1d30543e2f7da0137019cedeac996fdb088799ee
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-02-10 10:42:38 +0100

    modify index.ejs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 800
+const port = 8000
 let jsonFile = require('jsonfile');
 app.set('views', "./views")
 app.set("view engine", "ejs")
