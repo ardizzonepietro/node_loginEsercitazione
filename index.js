2aa877af1ca152d4ec4c6054738b9ef45b5f2c8c
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-02-10 11:13:29 +0100

    finish

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express()
 const port = 8000
+app.use(express.urlencoded({extended : false}))
 let jsonFile = require('jsonfile');
+var fs = require('fs');
+
+
+//setViews
 app.set('views', "./views")
 app.set("view engine", "ejs")
 
@@ -9,8 +14,25 @@ app.get("/", (req, res)=>{
   res.render("index.ejs")
   
 })
-app.post("/index.ejs", (req, res)=>{
-console.log("user: "+req.body.user+" pw: "+req.body.pw)
 
+
+//post login
+app.post("/index.ejs", (req, res)=>{
+    mail = req.body.mail;   
+    var fs = require('fs')
+     
+    var currentSearchResult = 'mail: '+mail;
+    
+    fs.readFile('data/utente.json', function (err, data) {
+        var json = JSON.parse(data)
+        json.push('search result: ' + currentSearchResult)
+    
+        fs.writeFile("results.json", JSON.stringify(json))
+    })
+   
 })
+
+
+
+//listen
 app.listen(port, ()=>console.info("sto ascoltandando"))
